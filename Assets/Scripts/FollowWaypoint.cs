b3628e4a121876e141f8769672d5ff141d356955
RodrigoSilva_a59659 <a59659@alunos.ipb.pt>
2025-10-16 17:02:04 +0100

    Direction of Travel

--- Assets/Scripts/FollowWaypoint.cs
+++ Assets/Scripts/FollowWaypoint.cs
@@ -8,13 +8,14 @@ public class FollowWaypoint : MonoBehaviour
     int currentWP = 0;
 
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
 
     void Start()
     {
         
     }
 
-    void Update()
+    void Update() 
     {
         if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
             currentWP++;
@@ -22,7 +23,11 @@ public class FollowWaypoint : MonoBehaviour
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+        //this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
