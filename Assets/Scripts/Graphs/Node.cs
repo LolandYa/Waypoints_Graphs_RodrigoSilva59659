52f5d4b6bc4b023d6202e13196ae27417daf9149
RodrigoSilva_a59659 <a59659@alunos.ipb.pt>
2025-10-16 19:30:49 +0100

    Simple Graph2/3

--- Assets/Scripts/Graphs/Node.cs
+++ Assets/Scripts/Graphs/Node.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class Node 
 {
-    public List<Edge> edgelist = new List<Edge>();
+    public List<Edge> edgeList = new List<Edge>();
     public Node path = null;
     GameObject id;
-    public float xPos;
-    public float yPos;
-    public float zPos;
+  
     public float f, g, h;
     public Node cameFrom;
     public Node(GameObject i)
     {
         id = i;
-        xPos = i.transform.position.x;
-        yPos = i.transform.position.y;
-        zPos = i.transform.position.z;
         path = null;
     }
     public GameObject getId()
